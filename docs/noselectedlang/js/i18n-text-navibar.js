873f49924cdb58078f909469d49a970dc6f3e56a
Lin, Kao-Yuan <mosdeo@gmail.com>
2020-12-10 17:30:29 +0800

    抽取 navibar 的 i18n 文本到同一個 js

---
+++ docs/noselectedlang/js/i18n-text-navibar.js
@@ -0,0 +1,40 @@
+// use plugins and options as needed, for options, detail see
+// http://i18next.com/docs/
+i18next.init({
+  lng: getCurrentLangByURLAutoFixToSupported(), // evtl. use language-detector https://github.com/i18next/i18next-browser-languageDetector
+  resources: { // evtl. load via xhr https://github.com/i18next/i18next-xhr-backend
+    'zh-CN': {
+        translation: { 
+            nav: {
+                title: 'LKY\'s 小红点 - 官方网站',
+                buy: '哪里买才得到？',
+                whyisitsoexpensive: '为什么这么贵？',
+                salesdataanalysisgallery: '销售数据分析艺廊',
+            }
+        }
+    }, 
+    'zh-TW': {
+        translation: {
+            nav: {
+                title: 'LKY\'s 小紅點 - 官方網站',
+                buy: '哪裡買才得到？',
+                whyisitsoexpensive: '為什麼這麼貴？',
+                salesdataanalysisgallery: '銷售數據分析藝廊',
+            }
+        }
+    }, 
+    'en-US': {
+        translation: {
+            nav: {
+                title: 'LKY\'s Soft-Rim TrackPoint - WebStie',
+                buy: 'Buy',
+                whyisitsoexpensive: 'Why is it so expensive？',
+                salesdataanalysisgallery: 'Sales Data Analysis Gallery',
+            }
+        }
+    }
+  }
+}, function(err, t) {
+  localize = locI18next.init(i18next);
+  localize('.nav');
+});
