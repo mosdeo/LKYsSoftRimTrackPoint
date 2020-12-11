7997ec38b333d5affc74a78c9926a14f398835ed
Lin, Kao-Yuan <mosdeo@gmail.com>
2020-12-11 22:20:33 +0800

    網頁標題改簡短

--- docs/noselectedlang/js/i18n-text-navibar.js
+++ docs/noselectedlang/js/i18n-text-navibar.js
@@ -6,7 +6,7 @@ i18next.init({
     'zh-CN': {
         translation: { 
             nav: {
-                title: 'LKY\'s 小红点 - 官方网站',
+                title: 'LKY\'s 凹小红点',
                 buy: '哪里买才得到？',
                 whyisitsoexpensive: '为什么这么贵？',
                 salesdataanalysisgallery: '销售数据分析艺廊',
@@ -16,7 +16,7 @@ i18next.init({
     'zh-TW': {
         translation: {
             nav: {
-                title: 'LKY\'s 小紅點 - 官方網站',
+                title: 'LKY\'s 凹小紅點',
                 buy: '哪裡買才得到？',
                 whyisitsoexpensive: '為什麼這麼貴？',
                 salesdataanalysisgallery: '銷售數據分析藝廊',
@@ -26,7 +26,7 @@ i18next.init({
     'en-US': {
         translation: {
             nav: {
-                title: 'LKY\'s Soft-Rim TrackPoint - WebStie',
+                title: 'LKY\'s Soft-Rim TrackPoint',
                 buy: 'Buy',
                 whyisitsoexpensive: 'Why is it so expensive？',
                 salesdataanalysisgallery: 'Sales Data Analysis Gallery',
